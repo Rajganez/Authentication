064f5c979e38e8be39a254eaff0a9eee15370d44
Rajganez <ganezsouth@live.com>
2024-04-14 22:48:41 +0530

    Facts generator

---
+++ index.js
@@ -0,0 +1,28 @@
+async function todayFacts() {
+let response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/today');
+await response.json()
+.then((data) => {
+   let facts = document.querySelector('.factContent');
+   facts.innerHTML = data.text; 
+}).catch((err) => {
+    err = "Error fetching facts"
+    alert(err);
+});
+}
+async function randomFacts() {
+    let response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random');
+    await response.json()
+   .then((data) => {
+    let facts = document.querySelector('.factContent');
+    facts.innerHTML = data.text;
+
+})
+}
+document.getElementById('prevent').addEventListener('click',(e) => {
+e.preventDefault();
+})
+
+todayFacts();
+function generateFact(){
+    randomFacts();
+}
